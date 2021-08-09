11dff3c4343703ac547e5f7bdc78bc505d569353
Foliver335 <79032229+Foliver335@users.noreply.github.com>
2021-08-09 19:49:41 -0300

    Update main.py

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,40 @@
+import pandas as pd
 import pickle
 from fastapi import FastAPI
+import requests
+import os
+
+import requests
+
+response = requests.get("https://github.com/Foliver335/treinamento-dia-04-08-Foliver33/tree/master/tarefa/codigo/model")
+print(response)
+# 200 é bom, 401 é ruim
+
+import pickle
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
+
+
 @app.post('/model')
-## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+
+async def titanic(Sex: int, Age: float, Lifeboat: int, Pclass: int, response: Response):
+    # rb= ler em binarios
+    with open('Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
+
+        response.status_code = status.HTTP_200_OK
+    return {
+        "survived": bool check (titanic.predict([[Sex, Age, Lifeboat, Pclass]])) = true;
+        bool check = true;
+        Console.WriteLine(check ? "alive" : "not alive");
+        Console.WriteLine(false ? "alive" : "not alive");  
+        
+        "status": response.status_code,
+        "message": ' '
+    }
+
 
 @app.get('/model')
 def get():
-    return {'hello':'test'}
+    return {'hello': 'test'}
